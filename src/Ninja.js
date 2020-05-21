ad83445c0be1f15f91a5659ce01602721cd49ac4
widdesto.yudistiro <widdesto.yudistiro@metraplasa.co.id>
2020-05-21 14:41:25 +0700

    commit pertama

---
+++ src/Ninja.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const Ninja = ({ ninjaList,hapusNinja }) => {
+    const ninja = ninjaList.map(data => {
+
+        // if (data.nama !== 'desto') {
+        //     return (
+        //         <div className="ninjas" key={data.name + data.usia}>
+        //             <div>Nama : {data.nama} </div>
+        //             <div>Usia: {data.usia}</div>
+        //             <div>Status: {data.status}</div>
+        //         </div>
+        //     )
+        // }
+        // else {
+        //     return null
+        // }
+        return (
+            <div className="ninjas" key={data.name + data.usia}>
+                <div>Nama : {data.nama} </div>
+                <div>Usia: {data.usia}</div>
+                <div>Status: {data.status}</div>
+                <div><button onClick={ () => {hapusNinja(data.id)} }>hapus</button></div>
+            </div>
+        )
+
+    })
+    return (
+        <div>
+            {ninja}
+        </div>
+    )
+}
+
+export default Ninja;
